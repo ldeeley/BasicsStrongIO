3b0b66ffd2f426a7b2a516e1a1183d2dbc0e8fae
ldeeley <lester.deeley@yahoo.com>
2023-03-20 15:37:20 +0000

    rev words and chars

--- src/main/java/org/example/play/CountingVowelsAndConsonants.java
+++ src/main/java/org/example/play/CountingVowelsAndConsonants.java
@@ -1,8 +1,6 @@
 package org.example.play;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -15,16 +13,26 @@ public class CountingVowelsAndConsonants {
 
         String testString = "count the number of vowels and consonants in this sentence";
 
-        Predicate<Character> isAVowel = new Predicate<Character>() {
-            @Override
-            public boolean test(Character character) {
-                return "aeiouAEIOU".indexOf(character)!=-1;
-            }
-        };
-
-        Map<Boolean, Long> collect = testString.chars()
+        Optional<Map.Entry<Character, Long>> collect = testString.chars()
                 .mapToObj(x -> (char) x)
-                .collect(Collectors.partitioningBy(isAVowel,Collectors.counting()));
+                .filter(x-> !x.equals(' '))
+                .collect(Collectors.groupingBy(x -> x, Collectors.counting()))
+                        .entrySet()
+                                .stream()
+                                        .max(Map.Entry.comparingByValue());
+
+        collect.ifPresent(System.out::print);
+
+//        Predicate<Character> isAVowel = new Predicate<Character>() {
+//            @Override
+//            public boolean test(Character character) {
+//                return "aeiouAEIOU".indexOf(character)!=-1;
+//            }
+//        };
+//
+//        Map<Boolean, Long> collect = testString.chars()
+//                .mapToObj(x -> (char) x)
+//                .collect(Collectors.partitioningBy(isAVowel,Collectors.counting()));
 
 
 
